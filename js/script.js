1b6ffd80951e570394fdfb569b23b203d5af76cd
laura <lg8104@gmail.com>
2023-03-11 16:00:00 -0700

    Creating emoji repo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+document.addEventListener("keydown", function (e) {
+    var key = e.key;
+    if (key === "1") {
+        box1.innerText = "😺";
+    } else if (key === "2") {
+        box2.innerText = "🚀";
+    } else if (key === "3") {
+        box3.innerText = "💮";
+    }
+});
